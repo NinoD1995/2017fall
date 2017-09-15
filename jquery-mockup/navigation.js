62c595c9a30239d02fd197a116de53ba610f34f5
Anthony DiNardi <NinoD1995@gmail.com>
2017-09-14 23:47:36 -0400

    fixed small errors

--- jquery-mockup/navigation.js
+++ jquery-mockup/navigation.js
@@ -1,9 +1,9 @@
 $(function(){
     
-    $("#header-placeholder".load("_header.html", function () {
+    $("#header-placeholder").load("_header.html", function () {
         $(".nav-link").removeClass("active");
         $("#signup-link").addClass("active");
-    } ));
+    } );
 
 
     
